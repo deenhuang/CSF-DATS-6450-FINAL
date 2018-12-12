6d957bf88153e1d1a58bdfb3dd1d32ae606ac958
Deen Huang <43018739+deenhuang@users.noreply.github.com>
2018-12-11 21:20:09 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='csf group project',
-    version='1.0',
-    packages=['csf_project_folder'],
-    description = ("This package folder includes many modules for the computer science foundation final project")
-)
