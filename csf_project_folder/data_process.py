07bc524afa900d4c44efbf23fbedbd71d68656ea
Deen Huang <43018739+deenhuang@users.noreply.github.com>
2018-12-11 22:01:17 -0500

    Update data_process.py

--- csf_project_folder/data_process.py
+++ csf_project_folder/data_process.py
@@ -2,7 +2,7 @@ import pandas as pd
 import jieba
 
 
-# Load data:
+# Function to load data:
 def load_data_from_csv(file_name, header=0, encoding="utf-8"):
     data_df = pd.read_csv(file_name, header=header, encoding=encoding)
     
